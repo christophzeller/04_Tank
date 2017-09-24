7a5301ffcb03f06177fd152207a6869e96675af8
Christoph Zeller <christophzeller@users.noreply.github.com>
2017-09-24 22:47:52 +0200

    added initial project files

---
+++ PanzerArena/Source/PanzerArena/PanzerArenaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PanzerArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANZERARENA_API APanzerArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
